589685111fea0a5aae5f4140cb865a1b4dea9a4b
Mark <mark2004kyki@gmail.com>
2025-05-19 09:03:25 +0300

    latest working version

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from config.settings import db_params
 from database.models import Database
 import os 
 from dotenv import load_dotenv
+import ast
 load_dotenv()
-ADMIN_ID_LIST = int(os.getenv("ADMIN_ID"))
+ADMIN_ID_LIST = ast.literal_eval(os.getenv("ADMIN_ID_LIST"))
 
 if __name__ == "__main__":
     app = build_application()
     school_db = Database(db_params)
     #populate_admins
+    counter = 0 
     for admin_id in ADMIN_ID_LIST:
+        counter += 1
         school_db.insert_admin(
+            
             admin_id=admin_id,
-            admin_name="Admin",
-            admin_surname="Admin",
-            admin_username="Admin",
+            admin_name="Admin_" + str(counter),
+            admin_surname="Admin_" + str(counter),
+            admin_username="Admin_" + str(counter),
         )
     print("Bot works...")
     app.run_polling()
